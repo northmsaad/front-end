85f1ff98aac5242a24a6ddaae86b264a04512c73
northmsaad <msddr010@gmail.com>
2022-07-02 14:01:13 +0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>
+        <h2>Featured Product</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
