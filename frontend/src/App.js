17eada624b8bda93603ea1b767b6fb893ebb0687
northmsaad <msddr010@gmail.com>
2022-07-02 17:34:08 +0500

    5-video

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from "./data";
+
 function App() {
   return (
     <div>
@@ -5,8 +7,16 @@ function App() {
         <a href="/">Amazon</a>
       </header>
       <main>
-        <h2>Featured Product</h2>
-        data.product
+        <h1>Featured Product</h1>
+        <div className="products">
+          {data.products.map(product => (
+            <div className="product" key={product.slug}>
+              <img src={product.image} alt={product.name} />
+              <p>{product.name}</p>
+              <p>{product.price}</p>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
